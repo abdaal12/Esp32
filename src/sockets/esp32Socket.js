c8bfa2240e4920a15a20e73c29f180ee69718a1a
abdaal <aabdaal02@gmail.com>
2026-07-08 19:14:34 +0530

    wscommit

--- src/sockets/esp32Socket.js
+++ src/sockets/esp32Socket.js
@@ -10,6 +10,7 @@ const device = require("../controllers/deviceController");
 
 function handleEsp32Connection(ws) {
   if (device.isDeviceConnected()) {
+    console.log("Rejected a new ESP32 connection: one is already connected");
     ws.close(1008, "Another device already connected");
     return;
   }
@@ -18,15 +19,25 @@ function handleEsp32Connection(ws) {
   console.log("ESP32 connected");
   device.broadcastToFrontend(device.getStatus());
 
+  let chunkCount = 0;
+
   ws.on("message", (message, isBinary) => {
     if (isBinary) {
+      chunkCount++;
+      console.log(`Received audio chunk #${chunkCount}, size: ${message.length} bytes, recording=${device.isRecording()}`);
       device.appendAudioChunk(message);
     } else {
       console.log("ESP32 says:", message.toString());
     }
   });
 
-   ws.on("error", (err) => {
+  ws.on("close", (code, reason) => {
+    device.clearEsp32Socket();
+    console.log("ESP32 disconnected. Code:", code, "Reason:", reason.toString());
+    device.broadcastToFrontend(device.getStatus());
+  });
+
+  ws.on("error", (err) => {
     console.log("ESP32 socket error:", err.message);
   });
 }
